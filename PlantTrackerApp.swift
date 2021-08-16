6ca2888673cc6908bd0e44c1a86e1280a18edb54
dowden19 <59750775+dowden19@users.noreply.github.com>
2021-08-15 22:21:37 -0400

    Add files via upload

---
+++ PlantTrackerApp.swift
@@ -0,0 +1,20 @@
+//
+//  PlantTrackerApp.swift
+//  PlantTracker
+//
+//  Created by Jackson Dowden on 8/10/21.
+//
+
+import SwiftUI
+
+@main
+struct PlantTrackerApp: App {
+    
+    @StateObject var contentViewModel: ContentViewModel = ContentViewModel()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView().environmentObject(contentViewModel)
+        }
+    }
+}
